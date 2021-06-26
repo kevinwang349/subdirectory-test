06e9278af3e4d8855414edaca7013eb512c8afa7
kevinwang349 <53480441+kevinwang349@users.noreply.github.com>
2021-06-26 12:57:29 -0400

    Update index.js

--- event-listener-test/index.js
+++ event-listener-test/index.js
@@ -5,13 +5,17 @@ function init(){
     dropdown.setAttribute('name','test');
     let values=['one','two','three','test','yeet'];
     for(const value of values){
-        const option=document.createElement('option');
+        const option=document.createElement("option");
         option.innerHTML=value;
-        option.setAttribute('value',value);
-        option.addEventListener('click', dropdownSubmit);
+        option.setAttribute("value",value);
+        option.addEventListener("click", dropdownSubmit);
         dropdown.appendChild(option);
     }
     document.body.appendChild(dropdown);
+    let button=document.createElement("button");
+    button.innerHTML='test';
+    button.addEventListener("click", () => {console.log("test")});
+    document.body.appendChild(button);
 }
 
 function dropdownSubmit(value){
