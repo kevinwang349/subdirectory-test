9732f07f0b967a0c0f60073d9e8c39bf7a6d65ae
kevinwang349 <53480441+kevinwang349@users.noreply.github.com>
2021-06-25 21:14:19 -0400

    Create index.js

---
+++ event-listener-test/index.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded',init);
+
+function init(){
+    let dropdown=document.createElement('select');
+    dropdown.setAttribute('name','test');
+    let values=['one','two','three','test','yeet'];
+    for(const value of values){
+        const option=document.createElement('option');
+        option.innerHTML=value;
+        option.setAttribute('value',value);
+        option.addEventListener('click', dropdownSubmit(value));
+        dropdown.appendChild(option);
+    }
+    document.body.appendChild(dropdown);
+}
+
+function dropdownSubmit(value){
+    console.log(value);
+    /*let p=document.createElement('p');
+    p.innerHTML=value;
+    document.body.appendChild(p);*/
+}
